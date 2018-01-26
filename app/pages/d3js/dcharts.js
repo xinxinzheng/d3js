11c5880665dd062be11a6e2d4b7f74751579d5c9
zhengxinxin <zhengxinxin@360.cn>
2018-01-26 18:08:34 +0800

    all

--- app/pages/d3js/dcharts.js
+++ app/pages/d3js/dcharts.js
@@ -2,13 +2,13 @@
  * @Author: dupi
  * @Date: 2017-06-28 17:16:12
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-01-26 14:50:07
+ * @Last Modified time: 2018-01-26 17:30:45
  */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import d3 from 'd3'
-
+import * as d3 from 'd3'
+import "./d3.less";
 
 @connect(
   (state, props) => ({
@@ -25,20 +25,22 @@ export default class dcharts extends Component {
     }
   }
   
-  renderCharts(obj){
-    
+  renderCharts(id,obj){
+    console.log(d3);
+    let ele = d3.select("#" + id);
+    console.log(ele.style('color','red'));
   }
 
   componentDidMount() {
     setTimeout(() => {
-      // this.loadChart()
+      this.renderCharts('cylinder')
     }, 500)
   }
 
   render() {
     return (
       <div className="dcharts">
-        <div className="dcharts" />
+        <div className="cylinder" id="cylinder" />
       </div>
     )
   }
