401c9af9b2b93f8b5ae0d628e0db340f44aa9d24
zhengxinxin <zhengxinxin@b.360.cn>
2018-01-31 22:28:54 +0800

    all

--- app/pages/d3js/splashed.js
+++ app/pages/d3js/splashed.js
@@ -21,9 +21,20 @@ export default class SplashedDcharts extends Component {
         this.data =  [[0.5, 0.5],[0.7, 0.8],[0.4, 0.9],[0.11, 0.32],[0.88, 0.25],[0.75, 0.12],[0.5, 0.1],[0.2, 0.3],[0.4, 0.1]];
         this.padding = {left:40,right:20,top:20,bottom:20};
         this.addEle = this.addEle.bind(this); 
+        this.deleteEle = this.deleteEle.bind(this); 
+        this.updateEle = this.updateEle.bind(this); 
     }
 
     renderCharts() {
+        let ele = d3.select("#spalshed").append('svg').style('width','100%').style('height','100%');
+        this.drawAxis(ele);
+    }
+    updateCharts(){
+        let ele = d3.select("#spalshed").select('svg');
+        this.drawAxis(ele);
+    }
+
+    drawAxis(ele){
         let layout = d3.select('#spalshed');
         let w = parseInt(layout.style('width'));
         let h = parseInt(layout.style('height'));
@@ -35,54 +46,57 @@ export default class SplashedDcharts extends Component {
         })
         let xScale = d3.scaleLinear().domain([0,1]).range([this.padding.left,w - this.padding.right]);
         let xAxis = d3.axisBottom(xScale);
-        let yScale = d3.scaleLinear().domain([0,1]).range([h - this.padding.top,0]);
+        let yScale = d3.scaleLinear().domain([0,1]).range([h - this.padding.top,this.padding.bottom]);
         let yAxis = d3.axisLeft(yScale);
+        ele.append('g').attr('transform','translate(' + 0 + ',' + (h - this.padding.bottom) + ')').call(xAxis);
+        ele.append('g').attr('transform','translate(' + this.padding.left + ',' + 0 + ')').call(yAxis);
+        this.drawCircle(ele,xScale,yScale);
+    }
 
-        let ele = d3.select("#spalshed").append('svg').style('width','100%').style('height','100%');
-        let update = ele.select('circle').data(this.data);
+    drawCircle(ele,xScale,yScale){
+        let update = ele.selectAll('circle').data(this.data);
         let enter = update.enter();
         let exit = update.exit();
 
-        update.attr('cx',(d) => {
+        update.transition().duration(1000).attr('cx',(d) => {
             return xScale(d[0]);
         }).attr('cy', d => {
             return yScale(d[1]);
         }).attr('r',(d) => {
-            return (x + y) / 2 ;
+            return (d[0] + d[1]) / 2 * 10;
         })
-
-        enter.append('circle').attr('cx',(d) => {
-            console.log(xScale(d[0]));
+        enter.append('circle').attr('cx',d => {
+            return this.padding.left;
+        }).attr('cy',d => {
+            return 300 - this.padding.top;
+        }).transition().duration(1000).attr('cx',(d) => {
             return xScale(d[0]);
         }).attr('cy', d => {
-            console.log(xScale(d[1]));
             return yScale(d[1]);
         }).attr('r',(d) => {
-            return (d[0] + d[1]) / 2 ;
+            return (d[0] + d[1]) / 2 * 10;
         }).attr('fill',(d , i)=> {
             return this.colors[i]
         })
 
-        exit.remove();
-
-        ele.append('g').attr('transform','translate(' + 0 + ',' + (h - this.padding.bottom) + ')').call(xAxis);
-        ele.append('g').attr('transform','translate(' + this.padding.left + ',' + 0 + ')').call(yAxis);
-
+        exit.transition().duration(1000).attr('fill','white').remove();
     }
-
     //添加元素
     addEle(){
-        // let n = Math.random() * 150;
-        // this.data.push(n.toFixed(0));
-        // this.updateCharts();
+        let x = Math.random();
+        let y = Math.random();
+        this.data.push([x,y]);
+        this.updateCharts();
     }
 
     updateEle(){
-
+        this.data[0] = [0.8,0.9];
+        this.updateCharts();
     }
 
     deleteEle(){
-
+        this.data.pop();
+        this.updateCharts();
     }
 
     componentDidMount() {
@@ -96,6 +110,8 @@ export default class SplashedDcharts extends Component {
             <div className="spalshed">
                 <div className = "charts" id = "spalshed" />
                 <button onClick={this.addEle}>添加元素</button>
+                <button onClick={this.deleteEle}>移除元素</button>
+                <button onClick={this.updateEle}>更新元素</button>
             </div>
         )
     }
